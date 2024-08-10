359626c77444d8f2d02e584852d61c5a5300f38e
Phillip Navarrete <phillipvictor2000@gmail.com>
2024-08-10 19:09:32 +0100

    Added Neovim configs
    Signed-off-by: Phillip Navarrete <phillipvictor2000@gmail.com>

---
+++ nvim/lua/plugins/lsp-configs.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({})
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        -- see https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
+        ensure_installed = { "lua_ls", "tsserver", "html", "jdtls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.jdtls.setup({
+        capabilities = capabilities,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,9 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set clipboard+=unnamedplus")
+vim.cmd("set smartcase")
+vim.cmd("set incsearch")
+vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
