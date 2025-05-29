7bec0c084f91d4eef38154817f086d142a09e989
Phillip Navarrete <phillipvictor2000@gmail.com>
2025-05-29 17:41:48 +0100

    Added missed renaming of typescript server
    Signed-off-by: Phillip Navarrete <phillipvictor2000@gmail.com>

--- nvim/lua/plugins/lsp-configs.lua
+++ nvim/lua/plugins/lsp-configs.lua
@@ -10,7 +10,7 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         -- see https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
-        ensure_installed = { "lua_ls", "tsserver", "html", "jdtls" },
+        ensure_installed = { "lua_ls", "ts_ls", "html", "jdtls" },
       })
     end,
   },
